febba81ceb869764988b7fbb32228a7a27ad846a
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-05-09 23:41:30 +0300

    feat: switched the API to railway

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://node-course-jobs-api.onrender.com/api/v1';
+axios.defaults.baseURL = 'https://jobio-production.up.railway.app/api/v1';
 
 axios.interceptors.request.use(function (req) {
-  const user = localStorage.getItem('user');
+    const user = localStorage.getItem('user');
 
-  if (user) {
-    const { token } = JSON.parse(localStorage.getItem('user'));
-    req.headers.authorization = `Bearer ${token}`;
+    if (user) {
+        const { token } = JSON.parse(localStorage.getItem('user'));
+        req.headers.authorization = `Bearer ${token}`;
+        return req;
+    }
     return req;
-  }
-  return req;
 });
